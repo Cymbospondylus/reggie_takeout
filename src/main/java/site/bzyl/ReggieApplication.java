8e34e0326185338437750cfd9fb487ffab6fbf8b
Cymbospondylus <2810560593@qq.com>
2023-05-05 21:14:15 +0800

    DuplicateKeyExcetion

--- src/main/java/site/bzyl/ReggieApplication.java
+++ src/main/java/site/bzyl/ReggieApplication.java
@@ -1,12 +1,10 @@
 package site.bzyl;
 
-import com.alibaba.druid.support.logging.Log;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 @SpringBootApplication
